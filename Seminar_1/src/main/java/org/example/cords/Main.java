f3c7220fb92599fcdc4fc866a4b201c66e312c3a
nzzima <nikita-krylov-2012@mail.ru>
2024-01-28 10:58:41 +0300

    Added length method to cords Vector

---
+++ Seminar_1/src/main/java/org/example/cords/Main.java
@@ -0,0 +1,9 @@
+package org.example.cords;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector vect = new Vector(3,3,3);
+        System.out.println(vect);
+        System.out.println("vect.length() = " + vect.length());
+    }
+}
---
+++ Seminar_1/src/main/java/org/example/cords/Vector.java
@@ -0,0 +1,59 @@
+package org.example.cords;
+
+public class Vector {
+    private int x;
+    private int y;
+    private int z;
+
+    public Vector() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+    public Vector(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x = " + x +
+                ", y = " + y +
+                ", z = " + z +
+                '}';
+    }
+
+    /**
+     * @apiNote Метод, вычисляющий длину вектора
+     * @return Длина вектора
+     */
+    public double length() {
+        return Math.sqrt(x*x + y*y + z*z);
+    }
+}
