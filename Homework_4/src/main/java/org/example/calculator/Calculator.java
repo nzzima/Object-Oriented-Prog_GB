da5e7aace3b4a0c98adefb5eb71f666f0896b69e
Nikita Krylov <nikita-krylov-2012@mail.ru>
2024-02-08 17:23:24 +0300

    Added multiply and division methods

--- Homework_4/src/main/java/org/example/calculator/Calculator.java
+++ Homework_4/src/main/java/org/example/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package org.example.calculator;
 
+import com.sun.jdi.request.BreakpointRequest;
+
 import java.util.List;
 
 public class Calculator {
@@ -14,6 +16,34 @@ public class Calculator {
         double result = 0;
         for (Number number : numbers) {
             result += number.doubleValue();
+        }
+        return result;
+    }
+
+    /**
+     * Обобщенный метод умножения чисел
+     * @param numbers Список чисел
+     * @return Результат
+     */
+    public double multiply(List<? extends Number> numbers) {
+        double result = 1;
+        for (Number number : numbers) {
+            result *= number.doubleValue();
+        }
+        return result;
+    }
+
+    /**
+     * Обобщенный метод деления чисел {a, b} => b / a
+     * @param numbers 2 числа
+     * @return Результат
+     */
+    public double division(List<? extends Number> numbers) {
+        double result = 0;
+        if (numbers.get(0).doubleValue() == 0) {
+            throw new RuntimeException("На ноль делить нельзя!");
+        } else {
+            result = numbers.get(1).doubleValue() / numbers.get(0).doubleValue();
         }
         return result;
     }
--- Homework_4/src/main/java/org/example/calculator/Main.java
+++ Homework_4/src/main/java/org/example/calculator/Main.java
@@ -13,5 +13,20 @@ public class Main {
 
         List<Double> array2 = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.5));
         System.out.println("calculator.sum(array2) = " + calculator.sum(array2));
+
+        List<Integer> array3 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        System.out.println("calculator.multiply(array3) = " + calculator.multiply(array3));
+
+        List<Double> array4 = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.5));
+        System.out.println("calculator.multiply(array4) = " + calculator.multiply(array4));
+
+        List<Integer> array5 = new ArrayList<>(Arrays.asList(3, 9));
+        System.out.println("calculator.multiply(array5) = " + calculator.division(array5));
+
+        List<Double> array6 = new ArrayList<>(Arrays.asList(3.2, 15.5));
+        System.out.println("calculator.multiply(array6) = " + calculator.division(array6));
+
+        List<Double> array7 = new ArrayList<>(Arrays.asList(0.0, 15.5));
+        System.out.println("calculator.multiply(array7) = " + calculator.division(array7));
     }
 }
